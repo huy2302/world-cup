f99c6595cbc92a633acff6c27427107b9e6d639c
huydevtr <cushin2302@gmail.com>
2026-07-29 23:39:28 +0700

    update to deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   cleanDistDir: true,
-  telemetry: false,
+  // telemetry: false,
 };
 
 export default nextConfig;
